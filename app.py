de0b8bc7acf396211cc7c4ed875c9aa84ee2a50a
Jerson Martinez <jersonmartinezsm@gmail.com>
2021-09-09 13:45:58 -0600

    index and loyout, adding design in the main page

--- app.py
+++ app.py
@@ -21,9 +21,18 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+Dict_Phrases = [
+    {'Seas quien seas, hagas lo que hagas, cuando deseas con firmeza alguna cosa es porque este deseo nació en el alma del universo. Es tu misión en la tierra':'El Alquimista (Paulo Coelho)'},
+    {'Hay que tener mucho valor para oponernos a nuestros enemigos, pero mucho más para desafiar a nuestros amigos':'Harry Potter y la Piedra Filosofal'},
+    {'El mundo era tan reciente que muchas cosas carecían de nombre, y para nombrarlas había que señalarlas con el dedo':'Cien Años de Soledad (Gabriel García Márquez)'},
+    {'Amor y deseo son dos cosas diferentes; que no todo lo que se ama se desea, ni todo lo que se desea se ama':'Don Quijote de la Mancha (Miguel de Cervantes)'},
+    {'¡Qué maravilloso es que nadie necesite esperar ni un solo momento antes de comenzar a mejorar el mundo!':'El Diario de Ana Frank (Ana Frank)'},
+    {'No todo lo que es de oro reluce, ni toda la gente errante anda perdida':'El Señor de los Anillos (J.R.R. Tolkien)'}
+]
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", len = len(Dict_Phrases), Dict_Phrases = Dict_Phrases)
 
 if __name__ == '__main__':
     app.run(debug=False)
